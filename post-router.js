8e9d4fe17519246df5e26fdb6fc7221e2b589c7d
sam-malanchuk <malanchuksam@gmail.com>
2019-08-11 16:17:06 -0400

    added api endpoint for creating a new comment

--- post-router.js
+++ post-router.js
@@ -23,4 +23,23 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.post('/:id/comments', async (req, res) => {
+    const commentData = {...req.body, post_id: req.params.id };
+    if (commentData.text) {
+        try {
+            const comment = await Posts.insertComment(commentData);
+            res.status(201).json(comment);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                error: "There was an error while saving the comment to the database"
+            });
+        }
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide text for the comment."
+        });
+    }
+});
+
 module.exports = router;

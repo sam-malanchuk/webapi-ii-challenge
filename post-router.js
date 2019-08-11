0213b2cdad83a3c9a23042696410aa864efeb739
sam-malanchuk <malanchuksam@gmail.com>
2019-08-11 16:57:31 -0400

    added api endpoint to delete a specific post by ID

--- post-router.js
+++ post-router.js
@@ -92,4 +92,25 @@ router.get('/:id/comments', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const count = await Posts.remove(id);
+        if (count > 0) {
+            res.status(200).json({
+                message: 'The post has been deleted'
+            });
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "The post could not be removed"
+        });
+    };
+});
+
 module.exports = router;

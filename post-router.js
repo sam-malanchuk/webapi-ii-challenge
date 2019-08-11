9040a11f724d2ab7924e54a976f38e703b8e17f1
sam-malanchuk <malanchuksam@gmail.com>
2019-08-11 16:06:03 -0400

    added api endpoint for creating a new post

--- post-router.js
+++ post-router.js
@@ -4,4 +4,23 @@ const Posts = require('./data/db.js');
 
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    const postData = req.body;
+    if (postData.title && postData.contents) {
+        try {
+            const post = await Posts.insert(postData);
+            res.status(201).json(post);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                error: "There was an error while saving the post to the database"
+            });
+        };
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        });
+    }
+});
+
 module.exports = router;

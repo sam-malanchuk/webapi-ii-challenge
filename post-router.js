6b94c2028cf8f190b16466011bc24f2351c641d7
sam-malanchuk <malanchuksam@gmail.com>
2019-08-11 16:36:52 -0400

    added api endpoint to return post by specific ID

--- post-router.js
+++ post-router.js
@@ -54,4 +54,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await Posts.findById(id);
+        if (post.length > 0) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be retrieved." 
+        });
+    }
+});
+
 module.exports = router;
